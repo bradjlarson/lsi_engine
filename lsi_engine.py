9a69e2364ea9e44e0a0bdaf78172c2e5665c8657
Brad Larson <brad@shopsimply.me>
2013-09-26 10:36:45 -0400

    Updating functions

--- lsi_engine.py
+++ lsi_engine.py
@@ -5,75 +5,97 @@ import sys
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+#This assumes a query to a MySQL DB, but this can be changed
 def get_data(query, con):
 	with con:
 		cur = con.cursor(mdb.cursors.DictCursor)
 		cur.execute(query)
 		return cur.fetchall()
-				
+
+#This function assumes takes escaped text and returns the un-escaped form				
 def clean_text(dirty):
 	return unquote_plus(dirty)
-	
+
+#This is a generator that yields one DB result at a time	
 def prep_data(query, con):
 	data = get_data(query, con)
 	for row in data:
 		#cleaned = clean_text(row[article_text])
-		yield clean_text(row["article_text"])	
-	
-def to_texts(docs):
-	return [[word for word in doc.lower().split()] for doc in docs]
+		yield clean_text(row["article_text"])			
 
+#This function isn't working yet, but eventually it will remove all words that only appear once from the corpus
+def remove_singles(texts):
+	return [[word for word in text if word not in tokens_once] for text in texts]
+
+#this converts normal text documents (spaces and the like) to tokens		
+def to_texts(docs, stop_list):
+	texts = [[word for word in doc.lower().split() if word not in stop_list] for doc in docs]
+	return texts
+
+#this converts a collection of tokens to a bag of words count
 def to_dict(texts, filename=False):
 	dictnry = corpora.Dictionary(texts)
 	if filename:
 		dictnry.save('%s.dict' % filename)
 	return dictnry
 
+#this converts a collection of tokens to an id mapping based on a dictionary
 def to_corpus(dictnry, texts, filename=False):
 	corpus = [dictnry.doc2bow(text) for text in texts]
 	if filename:
 		corpora.MmCorpus.serialize('%s.mm' % filename, corpus)
 	return corpus
-	
-def get_corpus(query, con, filename=False):
+
+#this returns a corpus and dictionary based on a query and a connection
+#It also provides options for a stop list and the ability to save the dictionary and corpus	
+def get_corpus(query, con, stop_list=[], filename=False):
 	docs = prep_data(query, con)
-	texts = to_texts(docs)
+	texts = to_texts(docs, stop_list)
 	dictnry = to_dict(texts, filename)
 	return (to_corpus(dictnry, texts, filename), dictnry)
-	
+
+#this returns a TF-IDF model and a TF-IDF transformed corpus, with the option to save to filename	
 def build_tfidf(corpus, filename=False):
 	tfidf = models.TfidfModel(corpus)
 	if filename:
 		tfidf.save('%s.tfidf' % filename)
 	return (tfidf[corpus], tfidf)
-	
-def build_lsi(corpus, dictnry, filename=False, n_topics=200):
+
+#this returns TF-IDF and LSI models, a LSI transformed corpus and an index for searching, with the option to save to a filename	
+def build_lsi(corpus, dictnry, filename=False, n_topics=150):
 	(t_corpus, tfidf) = build_tfidf(corpus, filename)
 	lsi = models.LsiModel(t_corpus, id2word=dictnry, num_topics=n_topics)
-	index = similarities.MatrixSimilarity(lsi[t_corpus])
-	#index = similarities.Similarity('%s_shard' % filename, lsi[t_corpus], n_topics, num_best=10)
+	l_corpus = lsi[t_corpus]
+	#index = similarities.MatrixSimilarity(l_corpus)
+	index = similarities.Similarity('%s_shard' % filename, l_corpus, n_topics)
 	if filename:
 		lsi.save('%s.lsi' % filename)
 		index.save('%s.index' % filename)
-	return (tfidf, lsi, index)
-			
-def sort(thing):
-	return sorted(enumerate(thing), key=lambda item: -item[1])
+	return (l_corpus, tfidf, lsi, index)
 
-def transform_query(docs, dictnry, tfidf, lsi):
-	return lsi[tfidf[dictnry.doc2bow(docs)]]
+#returns a list with the n best matches in tuple form, requires you to pass in the objects
+def query_lsi(query, con, dictnry, tfidf, lsi, index, stop_list=[], num_matches=10):
+	data = to_texts(prep_data(query, con), stop_list)
+	data_bow = [dictnry.doc2bow(doc) for doc in data]
+	data_tfidf = tfidf[data_bow]
+	data_lsi = lsi[data_tfidf]
+	sims = [top_n(index[doc], num_matches) for doc in data_lsi]
+	return sims
 
-def query_lsi(query, con, dictnry, tfidf, lsi, index, n_results=10):
-	query_docs = prep_data(query, con)
-	print query_docs
-	results = sort(index[transform_query(query_docs, dictnry, tfidf, lsi)])
-	return results
-			
-def query_lsi_stored(query, con, filename, n_results=10):
+#returns a list with the n best matches in tuple form, loads the objects from disk
+def query_lsi_stored(query, con, filename, stop_list=[], num_matches=10):
 	dictnry = corpora.Dictionary.load('%s.dict' % filename)
 	tfidf = models.TfidfModel.load('%s.tfidf' % filename)
 	lsi = models.LsiModel.load('%s.lsi' % filename)
 	index = similarities.Similarity.load('%s.index' % filename)
-	query_docs = prep_data(query, con)
-	results = sort(index[transform_query(query_docs, dictnry, tfidf, lsi)])
-	return results
+	data = to_texts(prep_data(query, con), stop_list)
+	data_bow = [dictnry.doc2bow(doc) for doc in data]
+	data_tfidf = tfidf[data_bow]
+	data_lsi = lsi[data_tfidf]
+	sims = [top_n(index[doc], num_matches) for doc in data_lsi]
+	return sims
+
+#returns the n best matches	
+def top_n(query, n):	
+	sims = sorted(enumerate(query), key=lambda item: -item[1])
+	return sims[:n]
