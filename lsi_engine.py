532309847db0baedf97f187f22b76e06e00b7137
Brad Larson <brad@shopsimply.me>
2013-09-28 14:59:45 -0400

    Add dictionary filter mechanism

--- lsi_engine.py
+++ lsi_engine.py
@@ -162,8 +162,9 @@ def model_lsi_id(query, con, filename=False, stop_list=default_stop_list, n_topi
 
 #returns a list with the n best matches in tuple form, requires you to pass in the objects
 def query_lsi_id(query, con, dictnry, tfidf, lsi, index, id_mapping, stop_list=default_stop_list, num_matches=10):
-	texts = to_texts_id(prep_data(query, con), stop_list)
+	texts = to_texts_id(prep_data_id(query, con), stop_list)
 	(corpus, query_id_mapping) = to_corpus_id(dictnry, texts)
+	print query_id_mapping
 	corpus_tfidf = tfidf[corpus]
 	corpus_lsi = lsi[corpus_tfidf]
 	sims = [top_n(index[doc], num_matches) for doc in corpus_lsi]
@@ -177,8 +178,9 @@ def query_lsi_stored_id(query, con, filename, stop_list=default_stop_list, num_m
 	lsi = models.LsiModel.load('%s.lsi' % filename)
 	index = similarities.Similarity.load('%s.index' % filename)
 	id_mapping = cPickle.load(open('%s.idmap' % filename, 'rb'))
-	texts = to_texts_id(prep_data(query, con), stop_list)
+	texts = to_texts_id(prep_data_id(query, con), stop_list)
 	(corpus, query_id_mapping) = to_corpus_id(dictnry, texts)
+	print query_id_mapping
 	corpus_tfidf = tfidf[corpus]
 	corpus_lsi = lsi[corpus_tfidf]
 	sims = [top_n(index[doc], num_matches) for doc in corpus_lsi]
