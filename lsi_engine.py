b2d91503c288942a6ff8b16583866f0b5c7d9afd
Brad Larson <brad@shopsimply.me>
2013-09-30 13:07:07 -0500

    adding bayes classifier

--- lsi_engine.py
+++ lsi_engine.py
@@ -219,10 +219,12 @@ def classifer(con, sims, id_mapping, corpus, q_id_mapping, q_corpus, filename=Fa
 	probs = get_nb_probs(sql, con, id_mapping, corpus, q_id_mapping, q_corpus)
 	return probs
 	
-def save_results(probs):
+def save_results(con, probs):
+	with con:
+		cur = con.cursor(mdb.cursors.DictCursor)
 	for prob in probs:
 		sql = "insert into testing_preds (article_id, prediction) values (%s, %.4f)" % (prob[0], prob[1])
-		print sql			
+		cur.execute(query)				
 	
 #thought is to implement a dictionary to store the id_mappings, with the corpus number as the index
 #would then have another dictionary as the value, with keys for any number of values
