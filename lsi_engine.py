d09d2ada24e732add20f431702f34761b8326224
Brad Larson <brad@shopsimply.me>
2013-09-30 13:55:57 -0500

    adding bayes classifier

--- lsi_engine.py
+++ lsi_engine.py
@@ -193,7 +193,7 @@ def query_lsi_stored_id(query, con, filename, stop_list=default_stop_list, num_m
 	reverse_query_mapping = invert_dict(q_id_mapping)
 	sims = [top_n(index[doc], num_matches) for doc in corpus_lsi]
 	sims_id = {reverse_query_mapping[sims.index(sim)] : [(reverse_mapping[tup[0]], tup[1]) for tup in sim if tup[0] in reverse_mapping] for sim in sims if sims.index(sim) in reverse_query_mapping}
-	return (q_corpus, q_id_mapping, simprins_id)
+	return (q_corpus, q_id_mapping, sims_id)
 	
 def bridge_lsi_nb(sims, id_mapping, corpus, filename=False):
 	if filename:
