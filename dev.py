22f47a4f51f3aac82e787709f425855be868ceab
Brad Larson <brad@shopsimply.me>
2013-10-03 10:38:52 -0500

    adding cpickle save to probs

--- dev.py
+++ dev.py
@@ -16,9 +16,9 @@ o_corpus = _.corpora.MmCorpus('%s.mm' % filename)
 id_mapping = _.cPickle.load(open('%s.idmap' % filename, 'rb'))
 query = "select a.article_id, article_text from jobs.testing_corpus a, jobs.unique_likes b where a.article_id = b.article_id"
 #get the 100 most similar documents for each document queried against the model
-(q_corpus, q_id_mapping, sims_id) = _.query_lsi_stored_id(query, con, filename, num_matches=101)
+(q_corpus, q_id_mapping, sims_id) = _.query_lsi_stored_id(query, con, filename, num_matches=301)
 probs = _.classifier(con, sims_id, id_mapping, o_corpus, q_id_mapping, q_corpus)
-_.save_results(con, probs, 'num matches=101')
+_.save_results(con, probs, 'num matches=301')
 
 #things that i can adjust:
 #number of matches to use in NB
